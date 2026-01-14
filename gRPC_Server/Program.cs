6daa2418ac7971520787b88dc10259e6ba5b7c6d
Abubakr Mardonov <maxik010020003@gmail.com>
2026-01-14 17:37:52 +0500

    Удалены лишние файлы из репозитория

--- gRPC_Server/Program.cs
+++ gRPC_Server/Program.cs
@@ -2,23 +2,26 @@ using gRPC_Server.Services;
 using Microsoft.EntityFrameworkCore;
 using Infastructure.AutoMapper;
 using Serilog;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Настройка Serilog
+/*/ Настройка Serilog
 Log.Logger = new LoggerConfiguration() 
     .WriteTo.Console()
     .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
-builder.Host.UseSerilog();
+builder.Host.UseSerilog();*/
 
 // Add services to the container.
 builder.Services.AddGrpc();
 
 // In-memory database
-builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase("ProductsDb"));
-
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+    opt.UseNpgsql(connection));
 // AutoMapper registration
 builder.Services.AddAutoMapper(typeof(MapperProfile).Assembly);
 
--- gRPC_Server/obj/Debug/net9.0/gRPC_Server.AssemblyInfo.cs
+++ gRPC_Server/obj/Debug/net9.0/gRPC_Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gRPC_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cec46bbb532329403dca8c87801280012f0f5950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7288f6337d430bfdb8cb6f01b92fe30c262f4d41")]
 [assembly: System.Reflection.AssemblyProductAttribute("gRPC_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gRPC_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
